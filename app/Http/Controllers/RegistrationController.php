f37a0a8b139e8dcbb4d4394c9efabaf3fe42930b
FilipRadovic <f.radovic99@gmail.com>
2023-06-18 13:49:22 +0200

    Specification of registration controller

---
+++ app/Http/Controllers/RegistrationController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Registration;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+
+class RegistrationController extends Controller
+{
+    public function createRegistration() {}
+
+    public function getRegistrations() {}
+
+    public function getRegistration(string $id) {}
+
+    public function getRegistrationDocumentImage() {}
+}
