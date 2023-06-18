306a1bd0506d1af0026e5d3f902bd9923495a777
FilipRadovic <f.radovic99@gmail.com>
2023-06-18 14:39:34 +0200

    Added remaining migrations

---
+++ database/migrations/2023_06_18_123503_create_identification_documents_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('identification_documents', function (Blueprint $table) {
+            $table->id();
+            $table->string('document_number');
+
+            $table
+                ->foreignUuid('registration_id')
+                ->references('id')
+                ->on('registrations')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+
+            $table
+                ->foreign('type_id')
+                ->references('id')
+                ->on('identification_document_types')
+                ->cascadeOnUpdate()
+                ->restrictOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('identification_documents');
+    }
+};
---
+++ database/migrations/2023_06_18_123821_create_identification_document_images_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('identification_document_images', function (Blueprint $table) {
+            $table->id();
+            $table->binary('image');
+
+            $table
+                ->foreign('identification_document_id')
+                ->references('id')
+                ->on('identification_documents')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('identification_document_images');
+    }
+};
