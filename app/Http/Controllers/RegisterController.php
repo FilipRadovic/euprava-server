b89e261c1aa41f92fe49a3afa3c16ecb276154b6
Lola2001 <medenica9550@gmail.com>
2023-06-18 19:20:10 +0200

    implemented validation inside RegisterController __invoke method for creating new registration

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Password;
 
 class RegisterController extends Controller
 {
@@ -14,6 +15,26 @@ class RegisterController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        $request
+            ->validate([
+            'firstname' => 'bail|required|max:30',
+            'lastname' => 'bail|required|max:30',
+            'email' => 'bail|required|email',
+            'jmbg' => 'bail|required|digits:13',
+            'username' => 'bail|required|max:30',
+            'password' => [
+                'bail',
+                'confirmed',
+                Password::min(8)
+                    ->letters()
+                    ->mixedCase()
+                    ->numbers()
+                    ->symbols()
+                    ->uncompromised()
+            ],
+            'city' => 'bail|required|integer',
+            'document.type' => 'bail|required|integer',
+            'document.number' => 'bail|required|max:255',
+        ], [ "stopOnFirstFailure" => true]);
     }
 }
