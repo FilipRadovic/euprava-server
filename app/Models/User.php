b8e78e6b72bc4832988ae4727319b39e8f0c8e08
kosta39 <kokson65@gmail.com>
2023-06-25 15:10:45 +0200

    added jwt custom claim for user role

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,6 +46,8 @@ class User extends Authenticatable implements JWTSubject
      */
     public function getJWTCustomClaims()
     {
-        return [];
+        return [
+            'role' => $this->role
+        ];
     }
 }
